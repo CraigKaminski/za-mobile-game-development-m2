e7f5adf46194b6568d0b0430ae2f64e13d999866
Craig Kaminski <craig.j.kaminski@gmail.com>
2017-12-15 13:42:37 -0500

    Add chicken, horse, pig, and sheep images and display chicken in center of screen

--- src/states/Game.ts
+++ src/states/Game.ts
@@ -1,10 +1,15 @@
 export class Game extends Phaser.State {
   public preload() {
     this.game.load.image('background', 'images/background.png');
+    this.game.load.image('chicken', 'images/chicken.png');
+    this.game.load.image('horse', 'images/horse.png');
+    this.game.load.image('pig', 'images/pig.png');
+    this.game.load.image('sheep', 'images/sheep3.png');
   }
 
   public create() {
     const logo = this.game.add.sprite(0, 0, 'background');
-    // logo.anchor.setTo(0.5, 0.5);
+    const chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
+    chicken.anchor.setTo(0.5, 0.5);
   }
 }
