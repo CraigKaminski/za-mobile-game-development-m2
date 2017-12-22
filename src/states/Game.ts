1ccd65466b2fdf90665fae090afa599c916a08e1
Craig Kaminski <craig.j.kaminski@gmail.com>
2017-12-21 21:42:49 -0500

    Make game fill browser while maintaining aspect ratio and animal sprites private members of Game class

--- src/states/Game.ts
+++ src/states/Game.ts
@@ -1,4 +1,7 @@
 export class Game extends Phaser.State {
+  private chicken: Phaser.Sprite;
+  private horse: Phaser.Sprite;
+  private pig: Phaser.Sprite;
   private sheep: Phaser.Sprite;
 
   public preload() {
@@ -10,18 +13,22 @@ export class Game extends Phaser.State {
   }
 
   public create() {
-    const logo = this.game.add.sprite(0, 0, 'background');
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
 
-    const chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
-    chicken.anchor.setTo(0.5, 0.5);
-    chicken.scale.setTo(2, 1);
+    this.game.add.sprite(0, 0, 'background');
 
-    const horse = this.game.add.sprite(120, 10, 'horse');
-    horse.scale.setTo(0.5);
+    this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
+    this.chicken.anchor.setTo(0.5, 0.5);
+    this.chicken.scale.setTo(2, 1);
 
-    const pig = this.game.add.sprite(500, 300, 'pig');
-    pig.anchor.setTo(0.5);
-    pig.scale.setTo(-1, 1);
+    this.horse = this.game.add.sprite(120, 10, 'horse');
+    this.horse.scale.setTo(0.5);
+
+    this.pig = this.game.add.sprite(500, 300, 'pig');
+    this.pig.anchor.setTo(0.5);
+    this.pig.scale.setTo(-1, 1);
 
     this.sheep = this.game.add.sprite(100, 250, 'sheep');
     this.sheep.scale.setTo(0.5);
